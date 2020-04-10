3766ac6ed8b38fac0de8675e7a49ab400cfed03b
lin <l.i.n@abv.bg>
2020-04-10 21:59:18 +0300

    initial commit

--- src/main/java/project/imaginarium/service/models/offer/AccommodationServiceModel.java
+++ src/main/java/project/imaginarium/service/models/offer/AccommodationServiceModel.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import project.imaginarium.data.models.Planet;
 import project.imaginarium.data.models.Sector;
-import project.imaginarium.data.models.Tag;
+import project.imaginarium.data.models.offers.Tag;
+import project.imaginarium.data.models.users.Partner;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -16,13 +17,14 @@ public class AccommodationServiceModel {
 
     private String name;
     private String description;
+    private int days;
     private Sector sector;
     private Planet planet;
     private String picture;
     private List<Tag> tags;
-    private boolean isAllowedForAnimals;
     private BigDecimal pricePerAdult;
     private BigDecimal pricePerChildren;
+    private Partner provider;
 
     public AccommodationServiceModel() {
         this.tags = new ArrayList<>();
